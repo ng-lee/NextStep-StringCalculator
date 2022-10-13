37f52285bb9b38ac56285d2181cb8d3065a9f629
namgon <nglee.dev@gmail.com>
2022-10-13 18:14:10 +0900

    feat: 프로젝트 시작

---
+++ src/test/java/NextStep/StringCalculator/StringCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package NextStep.StringCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
