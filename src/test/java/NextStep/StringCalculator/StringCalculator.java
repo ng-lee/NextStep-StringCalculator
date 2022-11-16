41399414d9299b651fe2ecc2824e155a7b5c4de8
namgon <nglee.dev@gmail.com>
2022-11-16 20:57:08 +0900

    fix: calcalate -> calculate

--- src/test/java/NextStep/StringCalculator/StringCalculator.java
+++ src/test/java/NextStep/StringCalculator/StringCalculator.java
@@ -4,7 +4,7 @@ import java.util.stream.Stream;
 
 public class StringCalculator {
 
-    public int calcalate(String input) {
+    public int calculate(String input) {
         return Stream.of(input.split(",")).mapToInt(Integer::parseInt).sum();
     }
 }
--- src/test/java/NextStep/StringCalculator/StringCalculatorTest.java
+++ src/test/java/NextStep/StringCalculator/StringCalculatorTest.java
@@ -10,7 +10,7 @@ public class StringCalculatorTest {
     @DisplayName("\"1,2,3\"을 전달하는 경우, \"1 2 3\"을 출력")
     void getNumbers() {
         StringCalculator calculator = new StringCalculator();
-        int number = calculator.calcalate("1,2,3");
+        int number = calculator.calculate("1,2,3");
 
         Assertions.assertThat(number).isEqualTo(6);
     }
