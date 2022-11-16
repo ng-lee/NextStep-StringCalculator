47bb2f0d2377913f4a5d82f5c14928c514a5385e
namgon <nglee.dev@gmail.com>
2022-11-16 21:00:08 +0900

    refactor: test code

--- src/test/java/NextStep/StringCalculator/StringCalculatorTest.java
+++ src/test/java/NextStep/StringCalculator/StringCalculatorTest.java
@@ -1,26 +1,33 @@
 package NextStep.StringCalculator;
 
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class StringCalculatorTest {
+
+    StringCalculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new StringCalculator();
+    }
 
     @Test
     @DisplayName("\"1,2,3\"을 전달하는 경우, \"1 2 3\"을 출력")
     void getNumbers() {
-        StringCalculator calculator = new StringCalculator();
         int number = calculator.calculate("1,2,3");
 
-        Assertions.assertThat(number).isEqualTo(6);
+        assertThat(number).isEqualTo(6);
     }
 
     @Test
     @DisplayName("\"1,2,3,4,5,6\"을 전달하는 경우, 21을 출력")
     void getNumbers2() {
-        StringCalculator calculator = new StringCalculator();
         int result = calculator.calculate("1,2,3,4,5,6");
 
-        Assertions.assertThat(result).isEqualTo(21);
+        assertThat(result).isEqualTo(21);
     }
 }
