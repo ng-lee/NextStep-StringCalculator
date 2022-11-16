a017da7dd2aadac7e90a5cdac4064aa7cef5b182
namgon <nglee.dev@gmail.com>
2022-11-16 20:56:00 +0900

    refactor: 1,2,3을 전달하는 경우, 6을 출력

--- src/test/java/NextStep/StringCalculator/StringCalculator.java
+++ src/test/java/NextStep/StringCalculator/StringCalculator.java
@@ -1,12 +1,10 @@
 package NextStep.StringCalculator;
 
-import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class StringCalculator {
 
     public int calcalate(String input) {
-        int[] array = Stream.of(input.split(",")).mapToInt(Integer::parseInt).toArray();
-        return Arrays.stream(array).sum();
+        return Stream.of(input.split(",")).mapToInt(Integer::parseInt).sum();
     }
 }
